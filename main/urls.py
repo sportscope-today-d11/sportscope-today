e71c44879a5ec3fd40e327440b9fab3ea1c41b76
maknoon1a <ahmad.omar@ui.ac.id>
2025-10-24 22:25:42 +0700

    Fix player_detail URL path syntax

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ app_name = 'main'
 urlpatterns = [
     # PLAYER
     path('player',views.player_list ,name='player_list'),
-    path('player/<slug:slug>', views.player_detail, name='player_detail')
+    path('player/<slug:slug>', views.player_detail, name='player_detail'),
     path('', homepage, name="homepage"),
     path('news/', news_list, name='news_list'),
     path('news/<int:news_id>/', news_detail, name='news_detail'),
