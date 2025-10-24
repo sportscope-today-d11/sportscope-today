4261c50b25118d2753eef247cd4bc6f1f2db6fa2
Chris Darren Imanuel <chrisdarren05@gmail.com>
2025-10-24 20:41:16 +0700

    routing url dan menambahkan views

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.urls import path
-from main.views import *
+from . import views
 from django.conf.urls.static import static
 
 app_name = 'main'
 
 urlpatterns = [
-
+    # PLAYER
+    path('player',views.player_list ,name='player_list'),
+    path('player/<slug:slug>', views.player_detail, name='player_detail')
 ]
 
 if settings.DEBUG:
