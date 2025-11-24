f5cd65cbb242a4f5ce9e1e55970be3c496233726
raihanazizah <raihana.nur41@ui.ac.id>
2025-11-24 13:23:08 +0700

    update url matches

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('matches/history/', views.match_history, name='match_history'),
     path('matches/<str:date>/', views.matches_by_date, name='matches_by_date'),
     path('matches/detail/<uuid:match_id>/', views.match_detail, name='match_detail'),
+    path('matches/delete/<uuid:match_id>/', views.delete_match, name='delete_match'),
+
     path('api/matches_detail/', views.api_match_detail, name='api_match_detail'),
     path('api/matches_history/', views.api_match_history, name='api_match_history'),
     
